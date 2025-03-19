f99e49075c4dda31f72d14e00b4c5838a3025c80
MaxwellJChen <maxwelljchen1@gmail.com>
2025-03-19 14:24:12 -0500

    updated menu

--- src/main.c
+++ src/main.c
@@ -8,7 +8,7 @@
 #include "bowmaster.h"
 #include "snake.h"
 
-#define NUM_GAMES 2
+#define NUM_GAMES 3
 #define SCREEN_WIDTH 800
 #define SCREEN_HEIGHT 450
 #define min(a, b) ((a) < (b) ? (a) : (b))
@@ -30,20 +30,21 @@ static bool prev = false; // currently moving to previous game
 static bool next = false; // whether we are currently moving to the next game
 static float fade = 1.; // amount we are fading current image by
 static float EPS = 1e-9;
+static bool game_started = false;
 
 microbit_output_t m1, m2;
 
 void UpdateMenu() {
-	read_microbit(&m1);
+	// read_microbit(&m1);
 	// read_microbit(&m2);
-	print_microbit(&m1);
+	// print_microbit(&m1);
 	// print_microbit(&m2);
 	if(!prev && !next) {
-		if(IsKeyDown('P') || m1.A == 1 || m2.A == 1) {
+		if(IsKeyPressed('P') || m1.A == 1 || m2.A == 1) {
 			printf("PREV\n");
 			prev = true;
 		}
-		else if(IsKeyDown('N') || m1.B == 1 || m2.B == 1) {
+		else if(IsKeyPressed('N') || m1.B == 1 || m2.B == 1) {
 			printf("NEXT\n");
 			next = true;
 		}
@@ -62,15 +63,18 @@ void UpdateMenu() {
 		printf("FADING IN\n");
 		fade = min(fade + 0.02, 1.);
 	}
+	else if(IsKeyPressed(KEY_ENTER)) {
+		game_started = true;
+	}
 }
 
 void UpdateDrawFrameMenu(void) {
-	// BeginDrawing();
-    // ClearBackground(RAYWHITE);
-	// game_t* cur = games + current_game;
-	// DrawTexture(LoadTextureFromImage(cur->img), cur->img_x, cur->img_y, Fade(WHITE, max(0, fade)));
-	// DrawText(cur->name, cur->name_x, cur->name_y, 30, Fade(BLACK, max(0, fade)));
-	// EndDrawing();
+	BeginDrawing();
+    ClearBackground(RAYWHITE);
+	game_t* cur = games + current_game;
+	DrawTexture(LoadTextureFromImage(cur->img), cur->img_x, cur->img_y, Fade(WHITE, max(0, fade)));
+	DrawText(cur->name, cur->name_x, cur->name_y, 30, Fade(BLACK, max(0, fade)));
+	EndDrawing();
 }
 
 void resize_img(Image* img) {
@@ -79,19 +83,19 @@ void resize_img(Image* img) {
 
 int init() {
 	// Initialize microbits
-	m1.A = 0; m1.B = 0; m1.x = 0; m1.y = 0; m1.z = 0;
-	m2.A = 0; m2.B = 0; m2.x = 0; m2.y = 0; m2.z = 0;
-	strcpy(m1.port, "/dev/tty.usbmodem1102");
-	strcpy(m2.port, "/dev/tty.usbmodem1302");
+	// m1.A = 0; m1.B = 0; m1.x = 0; m1.y = 0; m1.z = 0;
+	// m2.A = 0; m2.B = 0; m2.x = 0; m2.y = 0; m2.z = 0;
+	// strcpy(m1.port, "/dev/tty.usbmodem1102");
+	// strcpy(m2.port, "/dev/tty.usbmodem1302");
 
-	if(configure_microbit(&m1) == -1) {
-		printf("Couldn't configure microbit1\n");
-		return -1;
-	}
-	if(configure_microbit(&m2) == -1){
-		printf("Couldn't configure microbit2\n");
-		return -1;
-	}
+	// if(configure_microbit(&m1) == -1) {
+	// 	printf("Couldn't configure microbit1\n");
+	// 	return -1;
+	// }
+	// if(configure_microbit(&m2) == -1){
+	// 	printf("Couldn't configure microbit2\n");
+	// 	return -1;
+	// }
 
 
 	// Initialize game information
@@ -114,6 +118,15 @@ int init() {
 	games[1].img_x = 130;
 	games[1].img_y = 80;
 
+	Image snake_img = LoadImage("snake2.png");
+	resize_img(&snake_img);
+	games[2].name = "Snake";
+	games[2].name_x = 350;
+	games[2].name_y = 25;
+	games[2].img = snake_img;
+	games[2].img_x = 130;
+	games[2].img_y = 80;
+
 	return 0;
 }
 
@@ -132,17 +145,25 @@ int main () {
 	
 	// game loop
 	char str[20];
-	InitGamePong(SCREEN_WIDTH, SCREEN_HEIGHT);
-	InitGameBM(SCREEN_WIDTH, SCREEN_HEIGHT);
-	InitGameSnake(SCREEN_WIDTH, SCREEN_HEIGHT);
+	// InitGamePong(SCREEN_WIDTH, SCREEN_HEIGHT);
+	// InitGameBM(SCREEN_WIDTH, SCREEN_HEIGHT);
 	while (!WindowShouldClose())		// run the loop until the user presses ESCAPE or presses the Close button on the window
-	{
-		UpdateDrawFrameSnake(&m1, &m2);
-
-		read_microbit(&m1);
-		print_microbit(&m1);
-		// UpdateMenu();
-		// UpdateDrawFrameMenu();
+	{	
+		if(game_started) {
+			if(current_game == 0) {
+				UpdateDrawFramePong(&m1, &m2);
+			}
+			else if(current_game == 1) {
+				UpdateDrawFrameBM(&m1, &m2);
+			}
+			else if(current_game == 2) {
+				UpdateDrawFrameSnake(&m1, &m2);
+			}
+		}
+		else {
+			UpdateMenu();
+			UpdateDrawFrameMenu();
+		}
 	}
 
 	close(m1.fd);
